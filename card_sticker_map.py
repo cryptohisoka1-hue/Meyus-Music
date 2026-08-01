cec40f2d0934097926e350f0036c12cce77d0804
cryptohisoka1-hue <cryptohisoka1@gmail.com>
2026-08-01 21:41:37 +0300

    Create card_sticker_map.py

---
+++ card_sticker_map.py
@@ -0,0 +1,4 @@
+# Kart kodu -> sticker paketindeki index (0'dan baslar) eslesmesi.
+# /stickerlar komutunun ciktisina bakarak bu sozlugu doldur.
+# Ornek: {"kirmizi_0": 0, "kirmizi_1": 1, ..., "wild_renk": 48, "wild_artidort": 49}
+CARD_TO_STICKER_INDEX = {}
