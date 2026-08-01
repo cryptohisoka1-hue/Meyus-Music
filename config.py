5c47a2688efb809d53d919be494118fc0096da65
cryptohisoka1-hue <cryptohisoka1@gmail.com>
2026-08-01 05:08:08 +0300

    Clean up comments in config.py
    Removed commented lines about Telegram Bot Token.

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-'''# Telegram Bot Token'ınızı buraya yazın
-# @BotFather'dan alabilirsiniz
+# Telegram Bot Token'ınızı buraya yazın
 BOT_TOKEN = "8832238568:AAH2f7d9UF2rxbRWjcXgnQcChzSvD8C1eKE"
 
 # Veritabanı dosya adı
 DATABASE_NAME = "uno_games.db"
-'''
