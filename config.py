747965f77d172fdb4f68a7b5b33f60334022814a
Hisoka <cryptohisoka1@gmail.com>
2026-08-04 15:31:36 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-DATABASE_NAME = "uno_games.db"
-# Depo sohbeti ID'si (kart önbellekleme için)
-# None bırakırsanız oyun grubu kullanılır
-_storage = os.getenv("STORAGE_CHAT_ID", "")
-STORAGE_CHAT_ID = int(_storage) if _storage else None
-
-STICKER_SET_NAME = os.getenv("STICKER_SET_NAME", "classic_colorblind")
